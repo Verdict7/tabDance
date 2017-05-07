5d0df1dd309bf1909ce504f11e3d03ad745de561
Verdict7 <vinct.dry@freenet.de>
2017-05-07 22:28:07 +0200

    Testing connection to server using jquery ajax

--- addon/popup/popup.js
+++ addon/popup/popup.js
@@ -1,6 +1,42 @@
-var onReady = function(tabs){console.log("Test"); console.log(tabs); };
+var onReady = function(tabs){
+  console.log("Test"); 
+  console.log(tabs); 
+
+  //Test Post request
+  var url = results.website + "tabs";
+  var data = [{ url : "facebook.com", timestamp : ((new Date()).toISOString()), position : 2}];
+
+	$.ajax({
+		url:url,
+		type:"POST",
+		data:JSON.stringify(data),
+		contentType:"application/json; charset=utf-8",
+		dataType:"json",
+		success: function(){
+      console.log("Finished Post"); 
+      console.log(result); 
+		}
+	})
+};
+
+var results; 
 var onErr = function(err){console.log("Some Err"); console.log(err); };
-var onStorageReady = function(result){console.log(result); var text = browser.tabs.query({}); text.then(onReady, onErr); };
+var onStorageReady = function(result){
+  results = result; 
+  console.log(result); 
+  var text = browser.tabs.query({}); 
+  text.then(onReady, onErr); 
+  if(result.website){ 
+    var url = result.website + "tabs";
+    console.log("Accessing URL: " + url); 
+    $.get(url, function(data){
+      console.log("Received data"); 
+      console.log(data); 
+    });
+  } else {
+    console.log("Site is missing!"); 
+  }
+};
 /*
 Listen for clicks in the popup.
 */
