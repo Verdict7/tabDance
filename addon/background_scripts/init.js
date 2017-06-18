472bbb4132f610a71876974c3d1f9151a70b00f1
Verdict7 <vinct.dry@freenet.de>
2017-06-18 14:46:02 +0200

    WIP

--- addon/background_scripts/init.js
+++ addon/background_scripts/init.js
@@ -1,6 +1,9 @@
 //Called when tab changes
 //Only handles url changes
 function handleUpdated(tabId, changeInfo, tabInfo) {
+  //Distinguish: URL change (clicked on link)
+  //Placement change (moved tab around) TODO
+  //Closed tab TODO
   if (changeInfo.url) {
 		console.log(tabInfo);
     console.log("Tab: " + tabId +
@@ -17,3 +20,32 @@ function handleCreated(tab) {
 }
 
 browser.tabs.onCreated.addListener(handleCreated);
+
+//Called when browser (window?) is closed TODO
+var handleClosed = function(){
+}
+
+
+var updateInterval = function(){
+  mergeRemote(); //Calls pushBacklog asynchronously
+}
+
+/**
+ * Get changes from remote that are newer than last update,
+ * merge changes if necessary.
+ */
+var mergeRemote = function(){
+  //Get changes
+  //Compare changes
+  //Merge changes
+}
+
+/**
+ * Use Rest api to push local changes to server
+ */
+var pushBacklog = function(){
+  //Read storage
+  var getting = browser.storage.local.get(["activityLog"]);
+  getting.then(onStorageReady, onErr);
+  //Push to remote
+}
