aa5de32e30b9c840c9bb751e01a868d5870db417
Verdict7 <vinct.dry@freenet.de>
2017-06-18 16:52:23 +0200

    WIP: bg script that collects data

--- addon/background_scripts/init.js
+++ addon/background_scripts/init.js
@@ -1,51 +1,113 @@
-//Called when tab changes
-//Only handles url changes
-function handleUpdated(tabId, changeInfo, tabInfo) {
-  //Distinguish: URL change (clicked on link)
-  //Placement change (moved tab around) TODO
-  //Closed tab TODO
-  if (changeInfo.url) {
-		console.log(tabInfo);
-    console.log("Tab: " + tabId +
-                " URL changed to " + changeInfo.url);
+//TODO: What happens when we have several windows?
+//TODO: Move tab between windows (onDetached)
+
+
+var backgroundFun = (function(){
+
+  function onErr(err){
+    console.log("Error!");
+    console.log(err);
   }
-}
-
-browser.tabs.onUpdated.addListener(handleUpdated);
-
-//Called when new tab is created
-function handleCreated(tab) {
-  console.log(tab.id);
-  console.log(tab.url);
-}
-
-browser.tabs.onCreated.addListener(handleCreated);
-
-//Called when browser (window?) is closed TODO
-var handleClosed = function(){
-}
 
 
-var updateInterval = function(){
-  mergeRemote(); //Calls pushBacklog asynchronously
-}
+  /** Update the sync info
+  /* @type The Type of update (create, delete, ...)
+   * @value The tab information
+   */
+  function setData(type, value){
+    function onStorageReady(result){
+      result.updateInformation = result.updateInformation || [];
+        result.updateInformation.push({type : type, value : value});
+          browser.storage.local.set({
+            updateInformation : result.updateInformation
+          });
+      }
+    var getting = browser.storage.local.get(["updateInformation"]);
+    getting.then(onStorageReady, onErr);
+  }
 
-/**
- * Get changes from remote that are newer than last update,
- * merge changes if necessary.
- */
-var mergeRemote = function(){
-  //Get changes
-  //Compare changes
-  //Merge changes
-}
+  function removeUpdateInformation(){
+    browser.storage.local.set({
+      updateInformation : []
+    });
+  }
 
-/**
- * Use Rest api to push local changes to server
- */
-var pushBacklog = function(){
-  //Read storage
-  var getting = browser.storage.local.get(["activityLog"]);
-  getting.then(onStorageReady, onErr);
-  //Push to remote
-}
+
+
+  //Called when tab changes
+  //Only handles url changes
+  function handleUpdated(tabId, changeInfo, tabInfo) {
+    //Distinguish: URL change (clicked on link)
+    if (changeInfo.url) {
+      console.log(tabInfo);
+      console.log("Tab: " + tabId +
+                  " URL changed to " + changeInfo.url);
+    }
+  }
+
+
+
+  //Called when tabs are closed
+  function handleRemoved(tabId, removeInfo){
+    console.log("Tab: " + tabId + " closed!");
+  }
+
+
+  //Called when tabs are moved
+  function handleMoved(tabId, moveInfo){
+    console.log("Tab: " + tabId + "moved from " + moveInfo.fromIndex + " to " + moveInfo.toIndex + ".");
+  }
+
+
+  //Called when new tab is created
+  function handleCreated(tab) {
+    console.log("New Tab with id " + tab.id + " created.");
+    console.log(tab.url);
+  }
+
+
+
+  var updateInterval = function(){
+    mergeRemote(); //Calls pushBacklog asynchronously
+  }
+
+  /**
+   * Get changes from remote that are newer than last update,
+   * merge changes if necessary.
+   */
+  var mergeRemote = function(){
+    //Get changes
+    //Compare changes
+    //Merge changes
+  }
+
+  /**
+   * Use Rest api to push local changes to server
+   */
+  var pushBacklog = function(){
+    //Read storage
+    var getting = browser.storage.local.get(["activityLog"]);
+    getting.then(onStorageReady, onErr);
+    //Push to remote
+  }
+
+  return {
+    handleCreated : handleCreated,
+    handleUpdated : handleUpdated,
+    handleMoved : handleMoved,
+    handleRemoved : handleRemoved,
+    updateInterval : updateInterval,
+    setData : setData,
+    removeUpdateInformation : removeUpdateInformation
+  }
+})();
+
+
+console.log("init bg");
+//Add the event listeners
+browser.tabs.onCreated.addListener(backgroundFun.handleCreated); //new tab
+browser.tabs.onUpdated.addListener(backgroundFun.handleUpdated); //new url
+browser.tabs.onMoved.addListener(backgroundFun.handleMoved); //move tab
+browser.tabs.onRemoved.addListener(backgroundFun.handleRemoved); //close tab
+
+//TODO Also add the interval that sends changes to server
